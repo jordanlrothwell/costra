9158b6abd02f38011bb2473b94a4fff6ee62ca4d
Jordan Rothwell <jordanlrothwell@gmail.com>
2022-04-25 16:19:40 +1000

    Small syntax error in Offer

--- server/models/schema/Offer.ts
+++ server/models/schema/Offer.ts
@@ -1,21 +1,19 @@
 import { Schema } from "mongoose";
 
 // sub-schema for the Matter model
-const offerSchema: object = new Schema(
-  {
-    isPlaintiff: {
-      type: Boolean,
-      required: true,
-    },
-    amount: {
-      type: Number,
-      required: true,
-    },
-    date: {
-      type: Date,
-      required: true,
-    },
+const offerSchema: object = new Schema({
+  isPlaintiff: {
+    type: Boolean,
+    required: true,
   },
-);
+  amount: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+});
 
 export default offerSchema;
